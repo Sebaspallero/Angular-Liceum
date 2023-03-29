b5f2114e0b8b26b19ba834033729fbd1a5e47d5c
Sebastian Pallero <sebastianoria@hotmail.com>
2023-03-29 20:49:37 -0300

    [feature] directives

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { student } from '../../models/students';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  public students: student[] = [
+    new student(1, "María", "Pérez", "mariaperez@gmail.com", 24, 'female', new Date('1999-05-20')),
+    new student(2, "Felipe", "Ventti", "feventti@gmail.com", 23, 'male', new Date('2000-07-04')),
+    new student(3, "Luis", "Sagaspi", "luiggisagaspi@gmail.com", 18, 'male', new Date('2005-11-26')),
+    new student(4, "Pablo", "Roca", "rocapablo@gmail.com", 25, 'male', new Date('1998-03-02')),
+    new student(5, "Fernanada", "Amado", "feramado@gmail.com", 20, 'female', new Date('2003-07-10')),
+  ]
 }
---
+++ src/app/models/students.ts
@@ -0,0 +1,11 @@
+export class student {
+    constructor(
+      public id: number,
+      public firstName: string,
+      public lastName: string,
+      public email: string,
+      public age: number,
+      public gender: string,
+      public birthdate: Date,
+    ) {}
+  }
